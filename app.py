b1a9730f71f3301946611057cc0bff24664950c5
geniusman420 <jacksopomc@gmail.com>
2023-11-30 18:12:17 +0000

    saving for if the next thing doesn't fucking work

--- app.py
+++ app.py
@@ -7,15 +7,24 @@ from config import db, sqlite_db_path
 from extensions import argon2
 from flask_limiter import Limiter
 from flask_less import lessc
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+
+
 argon2.init_app(app)
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_db_path
 app.config["SECRET_KEY"] = "havoc3141"
 
+
 lessc(app)
 
+
 db.init_app(app)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -27,6 +36,18 @@ limiter = Limiter(
     key_func=lambda: request.remote_addr,
     storage_uri="memory://"
 )
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+app.logger.handlers.clear()
+file_handler = RotatingFileHandler(
+    'error.log', maxBytes=1024 * 1024 * 10, backupCount=10)
+file_handler.setLevel(logging.ERROR)
+formatter = logging.Formatter(
+    '\n%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 @login_manager.user_loader
@@ -42,7 +63,23 @@ def load_user(user_id):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", active_page=request.path)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = argon2.generate_password_hash(form.password.data)
+        query = text(
+            "INSERT INTO users (username, password) VALUES (:username, :password)")
+        db.session.execute(
+            query, {"username": form.username.data, "password": hashed_password})
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -71,20 +108,14 @@ def logout():
     return redirect("/")
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    form = RegisterForm()
+@app.route('/courses')
+def courses():
+    pass
 
-    if form.validate_on_submit():
-        hashed_password = argon2.generate_password_hash(form.password.data)
-        query = text(
-            "INSERT INTO users (username, password) VALUES (:username, :password)")
-        db.session.execute(
-            query, {"username": form.username.data, "password": hashed_password})
-        db.session.commit()
-        return redirect(url_for('login'))
 
-    return render_template("register.html", form=form)
+@app.route('/tutors')
+def tutors():
+    pass
 
 
 @app.route("/profile/<username>")
@@ -104,6 +135,18 @@ def user_profile(username):
 @login_required
 def dashboard():
     return render_template("dashboard.html", user=current_user, username=current_user.username, uid=current_user.uid)
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html', error_code=403)
+
+
+@app.errorhandler(Exception)
+def handle_exception(error):
+    error_message = str(error)
+    error.code = 403
+    return render_template('error.html', error_code=error.code), error.code
 
 
 @app.errorhandler(429)
